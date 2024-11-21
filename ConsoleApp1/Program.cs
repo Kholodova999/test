229c7984cdca05780465f849f351e85204dd8022
kholodova6@mail.ru <kholodova6@mail.ru>
2024-11-21 18:00:02 +0300

    1

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,57 @@
+﻿using System;
+
+class Calculator
+{
+    static void Main()
+    {
+        double num1, num2, result;
+        char operation;
+
+        Console.WriteLine("Введите первое число:");
+        num1 = Convert.ToDouble(Console.ReadLine());
+
+        Console.WriteLine("Введите второе число:");
+        num2 = Convert.ToDouble(Console.ReadLine());
+
+        Console.WriteLine("Выберите операцию (+, -, *, /):");
+        operation = Console.ReadKey().KeyChar;
+        Console.WriteLine();
+
+        switch (operation)
+        {
+            case '+':
+                result = num1 + num2;
+                Console.WriteLine($"{num1} + {num2} = {result}");
+                break;
+
+            case '-':
+                result = num1 - num2;
+                Console.WriteLine($"{num1} + {num2} = {result}");
+                break;
+
+            case '*':
+                result = num1 * num2;
+                Console.WriteLine($"{num1} * {num2} = {result}");
+                break;
+
+            case '/':
+                if (num2 != 0)
+                {
+                    result = num1 / num2;
+                    Console.WriteLine($"{num1} / {num2} = {result}");
+                }
+                else
+                {
+                    Console.WriteLine("Ошибка: Деление на ноль невозможно.");
+                }
+                break;
+
+            default:
+                Console.WriteLine("Недопустимая операция.");
+                break;
+        }
+
+        Console.WriteLine("Нажмите любую клавишу для выхода.");
+        Console.ReadKey();
+    }
+}
